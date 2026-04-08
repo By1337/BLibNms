aa406f95f7eb2a73058b17030614fd2c3f0ab7cf
by1337 <by133700@gmail.com>
2026-04-09 00:37:24 +0300

    feat: forEachContentAndClear 1.21.8

--- src/main/java/dev/by1337/core/impl/bridge/world/BlockEntityUtilImpl.java
+++ src/main/java/dev/by1337/core/impl/bridge/world/BlockEntityUtilImpl.java
@@ -6,15 +6,22 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.ProblemReporter;
 import net.minecraft.world.Clearable;
+import net.minecraft.world.Container;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.storage.TagValueInput;
 import org.bukkit.Location;
+import org.bukkit.craftbukkit.CraftWorld;
 import org.bukkit.craftbukkit.block.CraftBlock;
-import org.bukkit.craftbukkit.block.CraftBlockEntityState;
+import org.bukkit.craftbukkit.inventory.CraftItemStack;
+import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
 
 public class BlockEntityUtilImpl implements BlockEntityUtil {
 
@@ -59,5 +66,22 @@ public class BlockEntityUtilImpl implements BlockEntityUtil {
         CraftBlock cb = (CraftBlock) location.getBlock();
         BlockEntity entity = cb.getCraftWorld().getHandle().getBlockEntity(cb.getPosition());
         if (entity instanceof Clearable clearable) clearable.clearContent();
+    }
+
+    private static final BlockPos.MutableBlockPos cachedPos = new BlockPos.MutableBlockPos();
+
+    @Override
+    public void forEachContentAndClear(Location location, Consumer<ItemStack> consumer) {
+        var level = ((CraftWorld) location.getWorld()).getHandle();
+        cachedPos.set(location.getBlockX(), location.getBlockY(), location.getBlockZ());
+        var entity = level.getBlockEntity(cachedPos);
+        if (entity == null) return;
+        if (!(entity instanceof Container c)) return;
+        var list = c.getContents();
+        if (list.isEmpty()) return;
+        for (var stack : list) {
+            consumer.accept(CraftItemStack.asCraftMirror(stack));
+        }
+        c.clearContent();
     }
 }
