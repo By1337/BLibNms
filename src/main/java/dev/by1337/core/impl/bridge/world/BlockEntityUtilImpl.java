1a4167cb7d10dc2cf6752ee2ab0deb1f6a1a4efc
by1337 <by133700@gmail.com>
2026-04-09 00:51:25 +0300

    feat: forEachContentAndClear

--- src/main/java/dev/by1337/core/impl/bridge/world/BlockEntityUtilImpl.java
+++ src/main/java/dev/by1337/core/impl/bridge/world/BlockEntityUtilImpl.java
@@ -5,12 +5,18 @@ import dev.by1337.core.impl.bridge.NMSUtil;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.Clearable;
+import net.minecraft.world.Container;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import org.bukkit.Location;
+import org.bukkit.craftbukkit.CraftWorld;
 import org.bukkit.craftbukkit.block.CraftBlock;
 import org.bukkit.craftbukkit.block.CraftBlockEntityState;
+import org.bukkit.craftbukkit.inventory.CraftItemStack;
+import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.function.Consumer;
 
 public class BlockEntityUtilImpl implements BlockEntityUtil {
 
@@ -51,5 +57,22 @@ public class BlockEntityUtilImpl implements BlockEntityUtil {
         CraftBlock cb = (CraftBlock) location.getBlock();
         BlockEntity entity = cb.getCraftWorld().getHandle().getBlockEntity(cb.getPosition());
         Clearable.tryClear(entity);
+    }
+
+    private static final BlockPos.MutableBlockPos cachedPos = new BlockPos.MutableBlockPos();
+
+    @Override
+    public void forEachContentAndClear(Location location, Consumer<ItemStack> consumer) {
+        var level = ((CraftWorld) location.getWorld()).getHandle();
+        cachedPos.set(location.getBlockX(), location.getBlockY(), location.getBlockZ());
+        var entity = level.getBlockEntity(cachedPos);
+        if (entity == null) return;
+        if (!(entity instanceof Container c)) return;
+        var list = c.getContents();
+        if (list.isEmpty()) return;
+        for (var stack : list) {
+            consumer.accept(CraftItemStack.asCraftMirror(stack));
+        }
+        c.clearContent();
     }
 }
